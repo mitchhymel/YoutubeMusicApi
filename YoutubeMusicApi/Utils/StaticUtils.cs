83d5c37c11f8767ce0c1a66514095102d08c8365
mitchhymel <mitchell.hymel@gmail.com>
2020-10-04 19:04:44 -0700

    downgrading project to be .net standard 2.0 so it can be used in musicbee plugin (can revisit this decision later)

---
+++ YoutubeMusicApi/Utils/StaticUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeMusicApi.Utils
+{
+    public class StaticUtils
+    {
+        public static List<string> StringSplit(string input, string splitStr)
+        {
+            List<string> res = new List<string>();
+
+            string toProcess = input;
+            while (toProcess.IndexOf(splitStr) > -1)
+            {
+                int index = toProcess.IndexOf(splitStr);
+                string sub = toProcess.Substring(0, index);
+                res.Add(sub);
+                toProcess = toProcess.Remove(0, index+1);
+            }
+
+            res.Add(toProcess);
+
+            return res;
+        }
+    }
+}
